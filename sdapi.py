82b5a35fca9114287726f33e1f7687f0036dee88
Thomas Lemberger <thomas.lemberger@embo.org>
2017-08-14 11:21:44 -0400

    removed unescape step since database was cleaned up

--- sdapi.py
+++ sdapi.py
@@ -42,39 +42,10 @@ class Util():
                 
     @staticmethod
     def quote4neo(attributes):
-        quotes_added = {k:'"{}"'.format(Util.unescape(v.encode('utf-8').replace("'",r"\'").replace('"',r'\"'))) if isinstance(v, basestring) else v for k,v in attributes.items()}
+        quotes_added = {k:'"{}"'.format(v.encode('utf-8').replace("'",r"\'").replace('"',r'\"')) if isinstance(v, basestring) else v for k,v in attributes.items()}
         #include some cleanup of HTML entities eg &nbsp;
         properties = ','.join(["{}:{}".format(k,v) for (k,v) in quotes_added.items()])
         return properties
-        
-
-    @staticmethod
-    def unescape(text):
-        ##
-        # from http://effbot.org/zone/re-sub.htm#unescape-html
-        # Removes HTML or XML character references and entities from a text string.
-        #
-        # @param text The HTML (or XML) source text.
-        # @return The plain text, as a Unicode string, if necessary.
-        def fixup(m):
-            text = m.group(0)
-            if text[:2] == "&#":
-                # character reference
-                try:
-                    if text[:3] == "&#x":
-                        return unichr(int(text[3:-1], 16))
-                    else:
-                        return unichr(int(text[2:-1]))
-                except ValueError:
-                    pass
-            else:
-                # named entity
-                try:
-                    text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
-                except KeyError:
-                    pass
-            return text # leave as is
-        return re.sub("&#?\w+;", fixup, text)
 
 class SD_item(object):
     
