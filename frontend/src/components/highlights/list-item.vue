7ef46aeba99ac86c6f990e941fc8e43379e03abe
Thomas Lemberger <thomas.lemberger@embo.org>
2020-06-04 08:51:13 +0200

    adding markdonw viz

--- frontend/src/components/highlights/list-item.vue
+++ frontend/src/components/highlights/list-item.vue
@@ -6,21 +6,20 @@
             h3 {{ article.title }} 
             el-row(type="flex" justify="space-between")
               small() Posted 
-                b {{ display_date(article.pub_date) }}
+                b {{ displayDate(article.pub_date) }}
                 |  on 
-                i {{ display_journal(article.journal) }}
+                i {{ displayJournal(article.journal) }}
                 b  doi:  
                 el-link(type="primary" :href="href(article.doi)" target="_blank") http://doi.org/{{ article.doi }} 
             p
-              small {{ author_list }}
-            // div(v-if="article.review_process.reviews.length > 0") 
+              small {{ authorList }}
             div(v-if="article.review_process")
               el-collapse(v-for="review in article.review_process.reviews")
-                el-collapse-item(:title="'Reviewed by ' + review.reviewed_by + ' | Reviewer #'+review.review_idx")
-                  small {{ review.text }}
+                el-collapse-item(:title="'Reviewed by ' + displayJournal(review.reviewed_by) + ' | Reviewer #'+review.review_idx")
+                  small(v-html="mdRender(review.text)")
               el-collapse(v-if="article.review_process.response")
                 el-collapse-item(title="Response to the Reviewers")
-                  small {{article.review_process.response}}
+                  small(v-html="mdRender(article.review_process.response.text)")
       el-row()
         el-col(:span="10")
           p
@@ -37,6 +36,9 @@
     el-divider
 </template>
 <script>
+
+import MarkdownIt from 'markdown-it'
+
 export default {
   props: {
     article: Object,
@@ -47,26 +49,37 @@ export default {
     }
   },
   methods: {
-      href(doi) {
-          return new URL(doi, "https://doi.org/").href
-      },
-      display_date(date_str) {
-          const date = new Date(date_str)
-          const year = date.getFullYear()
-          const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-          const month = months[date.getMonth()]
-          const day = date.getDay()
-          return month + ' ' + day + ', ' + year 
-      },
-      display_journal(key) {
-        const journal_labels = {
-          biorxiv: 'bioRxiv', medrxiv: 'medRxiv'
-        }
-        return journal_labels[key]
+    href(doi) {
+        return new URL(doi, "https://doi.org/").href
+    },
+    displayDate(date_str) {
+        const date = new Date(date_str)
+        const year = date.getFullYear()
+        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        const month = months[date.getMonth()]
+        const day = date.getDay()
+        return month + ' ' + day + ', ' + year 
+    },
+    displayJournal(key) {
+      const journal_labels = {
+        biorxiv: 'bioRxiv', 
+        medrxiv: 'medRxiv',
+        'review commons': 'Review Commons',
+        elife: 'eLife',
       }
+      return journal_labels[key]
+    },
+    mdRender (md_text) {
+      const md = new MarkdownIt({
+          html: true,
+          linkify: true,
+          typographer: true
+      })
+      return md.render(md_text)
+    }
   },
   computed: {
-    author_list () {
+    authorList () {
       return this.article.authors.map(author => `${author.surname} ${author.given_names}${(author.corresp=='yes'?'*':'')}`).join(', ')
     },
     info () {
