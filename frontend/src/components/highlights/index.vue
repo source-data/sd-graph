8d565675c8bfffd43b6b51bb52fb77d5f6902a30
Thomas Lemberger <thomas.lemberger@embo.org>
2020-06-13 18:30:07 +0200

    changing location of spinning wheel to avoid screen flickering when loading time is fast

--- frontend/src/components/highlights/index.vue
+++ frontend/src/components/highlights/index.vue
@@ -1,35 +1,17 @@
 <template lang="pug">
   div
-    div(v-if="loadingRecords")
-      p(style="text-align: center")
-        el-button(type="primary" :loading="true" round plain)
-    div(v-else="loadingRecords")
-      HighlitedListItem(:article="article" v-for="article in records")
+    HighlitedListItem(:article="article" v-for="article in records")
 </template>
 
 <script>
 import HighlitedListItem from './list-item.vue'
-import { mapState, mapGetters } from 'vuex'
+import { mapGetters } from 'vuex'
 
 export default {
   components: {
     HighlitedListItem,
   },
-  // methods: {
-  //   displayJournal(key) {
-  //     const low_key = key.toLowerCase()
-  //     const journal_labels = {
-  //       biorxiv: 'bioRxiv', 
-  //       medrxiv: 'medRxiv',
-  //       'review commons': 'Review Commons',
-  //       elife: 'eLife',
-  //       'embo press': 'EMBO Press',
-  //     }
-  //     return journal_labels[low_key]
-  //   },
-  // },
   computed: {
-    ...mapState('highlights', ['loadingRecords']),
     ...mapGetters('highlights', ['records']),
   },
 }
