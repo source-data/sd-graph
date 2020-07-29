a875e7aa7883f2f0e00a12a72889a0b579fe722c
Alejandro Riera <ariera@gmail.com>
2020-07-29 11:06:13 +0200

    avoid flicker while fast changing from one hypothesis to another, by showing the spinning wheel BUT not removing the old content, just graying it out

--- frontend/src/components/highlights/index.vue
+++ frontend/src/components/highlights/index.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
   div
-    div(v-if="loadingRecords")
-      el-button(circle plain type="primary" :loading="true" size="normal")
-    div(v-else="loadingRecords")
+    div(v-show="loadingRecords" )
+      el-row
+        el-col
+          el-button(circle plain type="primary" :loading="true" style="position: absolute; right: 0; top: 10px;")
+    div(:class="{'highlights-loading': loadingRecords}")
       div(v-if="records.length > 0")
         el-row(type="flex" class="row-bg" justify="space-between")
           el-col
@@ -30,3 +32,11 @@ export default {
   },
 }
 </script>
+
+<style lang="scss">
+.highlights-loading {
+  h1, h2, h3, h4, h5, h6, p, small, b, i, em, a, span, div {
+    color: #bbb;
+  }
+}
+</style>
