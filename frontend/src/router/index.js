8ee7297b6f1d4f4ee88624a28a4f36f57281ee30
Alejandro Riera <ariera@gmail.com>
2020-08-25 16:40:27 +0200

    frontend; add 404 page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -29,7 +29,15 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  {
+    path: '/*',
+    name: 'NotFound',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "NotFound" */ '../views/NotFound.vue')
+  },
 ]
 
 const router = new VueRouter({
---
+++ frontend/src/views/NotFound.vue
@@ -0,0 +1,4 @@
+<template lang="pug">
+  div(style="text-align:center")
+    h1 404 Page not found
+</template>
