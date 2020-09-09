070ba782a3a1bcd06e1315acdfffae467c73095b
Alejandro Riera <ariera@gmail.com>
2020-09-09 16:08:27 +0200

    frontend bugfix quick access tabs
    * select right tab on direct navigation to other tab than the default one

--- frontend/src/components/quick-access/index.vue
+++ frontend/src/components/quick-access/index.vue
@@ -2,29 +2,25 @@
   v-card(class="pa-5")
     v-card-title Discover preprints with one of these methods
     v-tabs(v-model="activeTab_")
-      v-tab(:value="tabs.REFEREED_PREPRINTS_TAB")
-        router-link(to="/refereed-preprints/review-commons")
-          v-icon(class="px-1") mdi-book-open-variant
-          | Refereed Preprints
-      v-tab(:value="tabs.COVID19_HYP_TAB")
-        router-link(to="/covid19/by-hyp")
-          v-icon(class="px-1") mdi-help-circle-outline
-          | COVID-19 hypotheses
-      v-tab(:value="tabs.COVID19_AUTOMAGIC_TAB")
-        router-link(to="/covid19/automagic")
-          v-icon(class="px-1") mdi-auto-fix
-          | Automagic COVID-19 selection
-      v-tab(:value="tabs.FULLTEXT_SEARCH")
-        router-link(to="/all/search")
-          v-icon(class="px-1") mdi-text-box-search-outline
-          | Search preprints
-      v-tab-item
+      v-tab(to="/refereed-preprints/review-commons")
+        v-icon(class="px-1") mdi-book-open-variant
+        | Refereed Preprints
+      v-tab(to="/covid19/by-hyp")
+        v-icon(class="px-1") mdi-help-circle-outline
+        | COVID-19 hypotheses
+      v-tab(to="/covid19/automagic")
+        v-icon(class="px-1") mdi-auto-fix
+        | Automagic COVID-19 selection
+      v-tab(to="/all/search")
+        v-icon(class="px-1") mdi-text-box-search-outline
+        | Search preprints
+      v-tab-item(value="/refereed-preprints/review-commons")
         QuickAccessByReviewingService
-      v-tab-item
+      v-tab-item(value="/covid19/by-hyp")
         QuickAccessByHyp(@change="onChangeByHyp")
-      v-tab-item
+      v-tab-item(value="/covid19/automagic")
         QuickAccessByAutomagic
-      v-tab-item
+      v-tab-item(value="/all/search")
         QuickAccessSearchBar(@submit="onSubmitSearch")
 </template>
 
@@ -66,6 +62,7 @@ export default {
       // to him and keep it in sync with whatevr our parent component passes down as prop.
       // This little trick allows the parent component to dictate which tab is now active
       // which in turn is determined by the current route
+      console.debug("watching activeTab", this.activeTab, this.activeTab_, val)
       if (this.activeTab_ !== val) {
         this.activeTab_ = val
       }
