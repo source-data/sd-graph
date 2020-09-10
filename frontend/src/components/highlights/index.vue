b31d8ac670385b8a3a518a1cde43897db390d11c
Alejandro Riera <ariera@gmail.com>
2020-09-10 12:08:04 +0200

    frontend: add visual feedback when content is loading

--- frontend/src/components/highlights/index.vue
+++ frontend/src/components/highlights/index.vue
@@ -1,8 +1,11 @@
 <template lang="pug">
   div
-    div(v-if="records.length > 0")
-      h1 {{ records.length }} results found:
-    p(v-else) No results
+    v-row(justify="center" v-if="loadingRecords")
+      v-progress-circular(:size="70" :width="7" color="purple" indeterminate)
+    div(v-else="loadingRecords")
+    v-container(v-if="records.length > 0" :class="{'highlights-loading': loadingRecords}")
+      h1 {{ records.length }} articles found:
+    h1(v-if="records.length == 0 && !loadingRecords") No results
     v-container(:class="{'highlights-loading': loadingRecords}")
       v-row(v-for="article in records" :key="article.id")
         v-col
@@ -28,8 +31,11 @@ export default {
 
 <style lang="scss">
 .highlights-loading {
-  h1, h2, h3, h4, h5, h6, p, small, b, i, em, a, span, div {
-    color: #bbb;
+  h1, h2, h3, h4, h5, h6, p, small, b, i, em, a, span, div, .v-card .v-card__text .text--primary {
+    color: #bbb !important;
+  }
+  .v-expansion-panel, .v-card {
+    background-color: #eeeeee !important;
   }
 }
 </style>
