da1e826b88d65d8541a52970d54b4f448995c8e0
Thomas Lemberger <thomas.lemberger@embo.org>
2020-09-14 18:27:27 +0200

    sorting properly automagic by rank

--- frontend/src/components/quick-access/by-automagic.vue
+++ frontend/src/components/quick-access/by-automagic.vue
@@ -3,3 +3,13 @@
     v-card-title Preprints are prioritized by enriching for studies with a multiplicity of experimental approaches. 
 </template>
 
+<script>
+
+export default {
+  beforeMount(){
+    this.$store.commit('highlights/setSortBy', {value: 'rank'})
+    this.$store.commit('highlights/setSortDirection', {value: 'asc'})
+    this.$store.commit('highlights/sortRecords')
+  }
+}
+</script>
