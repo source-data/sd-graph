5c70bcbb93dc636892e6c08e55ebd9296ab69e4a
Thomas Lemberger <thomas.lemberger@embo.org>
2020-09-16 16:44:53 +0200

    conflict

--- neoflask/queries.py
+++ neoflask/queries.py
@@ -168,7 +168,7 @@ class BY_REVIEWING_SERVICE(Query):
 
     code = '''
 // Using precomputed Viz nodes
-MATCH (paper:VizPaper {query: "by_reviewing_service"})
+MATCH (col:VizCollection {name: "refereed-preprints")-->(subcol:VizSubCollection)-->(paper:VizPaper {query: "by_reviewing_service"})
 WHERE DATETIME(paper.peer_review_date) > DATETIME($limit_date)
 OPTIONAL MATCH (paper)-[:HasInfo]->(info:VizInfo)
 WITH DISTINCT
@@ -236,9 +236,9 @@ class AUTOMAGIC(Query):
     code = '''
 // using precomputed Viz nodes
 MATCH
-  (paper:VizPaper {query: "automagic"}),
-  (paper)-[:HasInfo]->(:VizInfo {title: 'Experimental approaches'})-[:HasEntity]->(exp_assays:VizEntity),
-  (paper)-[:HasInfo]->(:VizInfo {title: 'Biological entities'})-[:HasEntity]->(biol_entities:VizEntity)
+  (col:VizCollection {name: "automagic"})-->(subcol:VizSubCollection {name: "covid19"})-->(paper:VizPaper),
+  (paper)-[:HasInfo]->(:VizInfo)-[:HasEntity]->(exp_assays:VizEntity {category: "assay"}),
+  (paper)-[:HasInfo]->(:VizInfo)-[:HasEntity]->(biol_entities:VizEntity {category: "entity"})
 WHERE DATETIME(paper.pub_date) > DATETIME($limit_date)
 WITH DISTINCT paper, exp_assays, biol_entities
 ORDER BY
@@ -248,126 +248,9 @@ WITH DISTINCT
   [{title: "Experimental approaches", text: "", entities: COLLECT(DISTINCT {text: exp_assays.text})},
    {title: "Biological entities", text: "", entities: COLLECT(DISTINCT {text: biol_entities.text})}] AS info
 WITH {doi: doi, info: info, pub_date: pub_date, rank: rank} AS paper
-RETURN 'automagic list' AS name, "1" AS id, COLLECT(paper) AS papers
+RETURN "automagic" AS name, "covid19" AS id, COLLECT(paper) AS papers
     '''
     map = {'limit_date': []}
-    returns = ['name', 'id', 'papers']
-
-
-class BY_METHOD(Query):
-    code = '''
-//pre listed methods
-UNWIND [
-  {name: 'electron microscopy',   regex: '.*electron micro.*'            },
-  {name: 'flow cytometry',        regex: '.*electron micro.*'            },
-  {name: 'crystal structures',    regex: '.*(crystallo|crystal struct).*'},
-  {name: 'immunoprecipitation',   regex: '.*immunoprecip.*'              },
-  {name: 'immunohistochemistry', regex: '.*immunohistoc.*'               },
-  {name: 'histology',             regex: '.*histol.*'                    },
-  {name: 'pseudotype entry',      regex: '.*pseudotype.*'                }
-] AS query
-MATCH (q:Term)<-[:Has_text]-(h:H_Entity {category: "assay"})
-WHERE 
-  q.text =~ query.regex
-  OR
-  h.name =~ query.regex
-WITH h, query.name AS name
-MATCH (h)-[:Has_text]->(syn1:Term)
-OPTIONAL MATCH
-   (h)-[:Has_text]->(:Term)<--(bridge:H_Entity {category: "assay"})-->(syn2:Term)
-WITH name, COLLECT (DISTINCT syn1.text) + [h.name, bridge.name]  as all_synonyms //COLLECT(syn2.text) + [bridge.name] 
-UNWIND all_synonyms AS syn
-WITH DISTINCT syn, name
-WHERE NOT syn IS NULL
-WITH syn, name
-MATCH (a:SDArticle {journalName:'biorxiv'})-->(f:SDFigure)-->(p:SDPanel)-->(ct:CondTag {category: "assay"})-->(h:H_Entity)-[:Has_text]->(te:Term {text: syn})
-WITH DISTINCT
-  name, {doi: a.doi, info: COLLECT(DISTINCT {id: id(p), title: f.fig_label, text: p.caption}), pub_date: a.pub_date} AS paper
-ORDER BY paper.pub_date DESC
-RETURN DISTINCT
-   name AS name, name AS id, COLLECT(paper) AS papers
-    '''
-    returns = ['name', 'id', 'papers']
-
-
-class BY_MOLECULE(Query):
-    code = '''
-//Exclusion list based on SourceData normalized entities
-MATCH (syn:Term)<--(mol:H_Entity)<--(ct:CondTag)
-WHERE 
-  mol.type = "small_molecule" OR mol.type = "molecule"
-  AND 
-  mol.ext_ids <> ""
-WITH DISTINCT mol.name AS name, COLLECT(DISTINCT syn.text) AS synonyms, COLLECT(DISTINCT ct) AS cts, 1.0*COUNT(DISTINCT ct) AS N
-UNWIND cts as ct
-WITH DISTINCT name, synonyms, N, ct.role as role, 1.0*COUNT(DISTINCT ct) AS N_role
-WITH name, synonyms, role, N, N_role, 100.0*(N_role / N) AS fract
-ORDER BY N DESC, fract DESC
-WITH name, synonyms, N, COLLECT(role)[0] AS dominant_role, COLLECT(fract)[0] AS dom_fract
-WHERE 
-  (dominant_role = "normalizing" OR dominant_role = "reporter" OR dominant_role = "component")
-  AND
-  dom_fract > 75 AND N > 10.0
-WITH COLLECT(name) + synonyms AS all
-UNWIND all as terms
-WITH COLLECT(DISTINCT terms) AS exclusion_list
-
-//
-MATCH
-  (query:H_Entity {category: "entity"})-[:Has_text]->(syn1:Term)
-WHERE 
-  (query.type = "molecule" OR query.type = "small_molecule")
-  //exclude known artefacts...
-  AND
-  (NOT syn1.text = "a" OR syn1.text = "-")
-  AND 
-  (NOT syn1.text IN exclusion_list)
-OPTIONAL MATCH
-  (query:H_Entity {category: "entity"})-[:Has_text]->(:Term)<-[:Has_text]-(secondary:H_Entity {category: "entity"})-[:Has_text]->(syn2:Term)
-WHERE 
-  (query.type = "molecule" OR query.type = "small_molecule")
-  AND
-  (secondary.type = "molecule" OR secondary.type = "small_molecule")
-  AND 
-  (NOT syn2.text IN exclusion_list)
-WITH DISTINCT [query, secondary] AS queries, syn1, syn2
-UNWIND queries AS query
-WITH DISTINCT query, syn1, syn2
-WHERE NOT query is NULL
-WITH DISTINCT query,  [query.name] + COLLECT(syn1.text) + COLLECT(syn2.text) AS all_synonyms
-UNWIND all_synonyms AS syn
-WITH DISTINCT query, syn
-ORDER BY syn
-WITH DISTINCT query, COLLECT(DISTINCT syn) AS synonym_sets
-ORDER BY size(query.name) DESC
-WITH DISTINCT COLLECT(DISTINCT query) AS query_group, synonym_sets
-UNWIND synonym_sets AS synonym
-WITH query_group, synonym
-
-MATCH
-  (paper:SDArticle)-->(f:SDFigure)-->(p:SDPanel)-->(ct:CondTag)-->(mol:H_Entity {category: "entity"})-[:Has_text]->(mol_name:Term {text: synonym})
-WHERE
-  toLower(paper.journalName) IN ["biorxiv", "medrxiv"]
-  AND
-  (mol.type = "molecule" OR mol.type = "small_molecule")
-  AND
-  datetime(paper.pub_date) > datetime('2020-04-01')
-WITH DISTINCT query_group, paper, f, p, synonym
-ORDER BY f.fig_label ASC
-WITH DISTINCT query_group, paper, COLLECT(DISTINCT {id: id(p), title: f.fig_label, text: p.caption}) AS panels, COUNT(DISTINCT p) AS N_panels, COLLECT(DISTINCT synonym) AS synonyms_in_paper
-WHERE 
-  N_panels > 2
-WITH DISTINCT
-  query_group,
-  COLLECT(DISTINCT {doi: paper.doi, title:paper.title, syn: synonyms_in_paper, info: panels, pub_date: paper.pub_date}) AS papers,
-  COUNT(DISTINCT paper) AS N_papers
-ORDER BY N_papers DESC
-LIMIT 20
-WITH
-  query_group[0].name AS molecule, papers
-RETURN
-  molecule AS name, molecule as id, papers
-'''
     returns = ['name', 'id', 'papers']
 
 
@@ -474,135 +357,3 @@ WITH COUNT(DISTINCT a.doi) AS autoannotated_preprints, refereed_preprints, biorx
 RETURN biorxiv_preprints, refereed_preprints, autoannotated_preprints
     '''
     returns = ['biorxiv_preprints', 'refereed_preprints', 'autoannotated_preprints']
-
-
-class PANEL_SUMMARY(Query):
-    code = '''
-// panel summary
-// Thi query provides a simplified summary of a panel.
-// Entities are listed in a simplified way for display.
-// Each entity has a primary label (i.e. the name of the entity),
-// a secondary label providing complementary info depending on entity type,
-// and a url pointing to an external knowledge base
-//
-// Args:
-//    panel_id: the panel id
-// Returns
-//    fig_label: the label of the parent figure
-//    fig_title: the title of the parent figure
-//    panel_label: the lable of the panel
-//    caption: the caption of the panel
-//    controlled_var: a list of entities linked to the controlled variable(s) of the experiment
-//    measured_var: a list of entities linked to the measured variable(s) of the experiment
-//    other: a list of other entities involved in the experiment
-//    methods: the experimental observation and measurement assays
-MATCH
-    (a:SDArticle)-->(f:SDFigure)-->(p:SDPanel {panel_id: $panel_id})
-WITH
-    a.doi AS doi,
-    f.fig_label AS fig_label,
-    p
-// find the matching figures from jats to extract the figure title
-MATCH
-    (ax:Article {doi:doi})-->(fx:Fig {label: fig_label})
-WITH 
-    doi,
-    fig_label,
-    fx.title as fig_title,
-    p.panel_label as panel_label,
-    p.caption as caption,
-    p
-// find the entities linked to the target panel
-MATCH
-    (p)-->(ct:CondTag), (resolver:Resolver)
-WHERE
-    ct.role <> "reporter" AND ct.role <>"normalizing" AND
-    ct.ext_dbs = resolver.name
-WITH
-    doi,
-    fig_label,
-    fig_title,
-    panel_label,
-    caption,
-    ct,
-    resolver
-// find the corresponding entities in the full paper to make a statistic for ranking
-MATCH
-    (a:SDArticle {doi: doi})-->(:SDFigure)--> (all_panels:SDPanel)-->(other_ct:CondTag {text:ct.text})
-WITH DISTINCT
-    doi,
-    fig_label,
-    fig_title,
-    panel_label,
-    caption,
-    ct,
-    resolver,
-    COUNT(DISTINCT other_ct) AS freq
-// combine entities with frequency into single list that can be processed
-WITH 
-    fig_label,
-    fig_title,
-    panel_label,
-    caption,
-    COLLECT(DISTINCT {
-        text: ct.text,
-        ext_ids: ct.ext_ids, 
-        role: ct.role,
-        type: ct.type,
-        category: ct.category, 
-        freq: freq, 
-        ext_tax_names: ct.ext_tax_names,
-        href: resolver.url + ct.ext_ids
-    }) AS entities
-// sort and aggregate depending on role
-WITH
-    [e IN entities WHERE (e['role']='intervention' OR e['role']='experiment') | e] AS controlled_entities,
-    [e IN entities WHERE e['role']='assayed' | e] AS measured_entities,
-    [e IN entities WHERE e['category']='assay' | e] AS assays,
-    [e IN entities WHERE (e['type']='component' OR e['category']='disease') | e] AS other_entities,
-    fig_label, fig_title, panel_label, caption
-// standardize the format of the output
-RETURN
-    [e in controlled_entities | {primary_label: e.text, secondary_label: e.ext_tax_names, href: e.href}] as controlled_var,
-    [e IN measured_entities WHERE e['role']='assayed' | {primary_label:e.text, secondary_label: e.ext_tax_names, href: e.href}] AS measured_var,
-    [e IN assays WHERE e['category']='assay' | {primary_label: e.text, secondary_label: "", href: e.href}] AS methods,
-    [e IN other_entities WHERE (e['type']='component' OR e['category']='disease') | {primary_label: e.text, secondary_label: e.ext_tax_names, href: e.href}] AS other,
-    fig_label, fig_title, panel_label, caption
-    '''
-    map = {'panel_id': []},
-    returns = ['fig_label', 'fig_title', 'panel_label', 'caption', 'controlled_var', 'measured_var', 'other', 'methods']
-
-
-class POPULAR_METHODS(Query):
-    code = '''
-// find most popular method with synonym aggregation
-MATCH (a:SDArticle {journalName:'biorxiv'})-->(f:SDFigure)-->(p:SDPanel)-->(ct:CondTag {category: "assay"})-->(h:H_Entity)
-WITH DISTINCT a, ct, h
-ORDER BY a.pub_date DESC
-WITH DISTINCT
-    COUNT(DISTINCT a) AS popularity,
-    COLLECT(DISTINCT a.doi)[..10] AS top10,
-    h
-ORDER BY popularity DESC
-MATCH (h)-->(t:Term)<--(h2:H_Entity)<--(ct:CondTag)<--(p:SDPanel)<--(f:SDFigure)<--(a:SDArticle)
-WITH DISTINCT
-    h, top10,
-    h2,
-    COUNT(DISTINCT a) AS N
-ORDER BY N DESC
-WITH DISTINCT
-    h.name AS synonym,
-    top10,
-    COLLECT(DISTINCT h2.name)[0] AS method
-UNWIND top10 AS paper
-WITH
-    synonym,
-    paper,
-    method
-RETURN
-    COLLECT(DISTINCT synonym) AS synonyms,
-    COLLECT(DISTINCT paper) AS papers,
-    method
-LIMIT 25
-    '''
-    returns = ['method', 'popularity']
