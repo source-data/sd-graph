55fde673e1bd5019eb9490a016e1accd33f53766
Thomas Lemberger <thomas.lemberger@embo.org>
2020-12-19 02:11:15 +0100

    changing base urls in sitemap

--- neoflask/sitemap.py
+++ neoflask/sitemap.py
@@ -2,7 +2,7 @@ from lxml import etree as ET
 import datetime
 
 
-BASE_URL = "https://eeb.sourcedata.io"
+BASE_URL = "https://eeb.embo.org"
 
 
 def add_url(root, loc, **kwargs):
@@ -26,16 +26,12 @@ def create_sitemap(dois):
     add_url(root, loc=f"{BASE_URL}/refereed-preprints/peerage-of-science", lastmod=dt, changefreq="monthly", priority="1.0")
     add_url(root, loc=f"{BASE_URL}/refereed-preprints/embo-press", lastmod=dt, changefreq="monthly", priority="1.0")
     add_url(root, loc=f"{BASE_URL}/refereed-preprints/elife", lastmod=dt, changefreq="monthly", priority="1.0")
-    add_url(root, loc=f"{BASE_URL}/covid19/by_hyp", lastmod=dt, changefreq="monthly", priority="1.0")
-    add_url(root, loc=f"{BASE_URL}/covid19/search", lastmod=dt, changefreq="monthly", priority="1.0")
+    add_url(root, loc=f"{BASE_URL}/refereed-preprints/rrc19", lastmod=dt, changefreq="monthly", priority="1.0")
+    add_url(root, loc=f"{BASE_URL}/refereed-preprints/peer-community-in", lastmod=dt, changefreq="monthly", priority="1.0"
+    add_url(root, loc=f"{BASE_URL}/all/auto-topics", lastmod=dt, changefreq="monthly", priority="1.0")
+    add_url(root, loc=f"{BASE_URL}/all/automagic", lastmod=dt, changefreq="monthly", priority="1.0")
 
     for doi in dois:
         add_url(root, loc=f"{BASE_URL}/doi/{doi}", priority="0.9")
     xml_document = ET.tostring(root, xml_declaration=True, encoding='utf-8').decode("utf-8")
     return xml_document
-
-# from .engine import Engine
-# from . import DB, app, cache
-# ASKNEO = Engine(DB)
-# refereed_preprints = ASKNEO.refereed_preprints()
-# import pdb;pdb.set_trace()
