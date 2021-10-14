a4ee199980f0bed0dabefa61f034ae52a7fdc5c2
Thomas Lemberger <thomas.lemberger@embo.org>
2021-10-14 07:35:51 +0200

    removing old element UI stuff

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './plugins/element.js'
 import VueMeta from 'vue-meta'
 import vuetify from './plugins/vuetify';
 
