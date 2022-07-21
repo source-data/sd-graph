0b6be63794879dd6a72dbd677b9042b76d070b0a
Thomas Eidens <thomas.eidens@embo.org>
2022-07-21 10:21:52 +0200

    Added peer review DOI import to db deployment

--- neotools/queries.py
+++ neotools/queries.py
@@ -16,3 +16,18 @@ class CREATE_INDEX_DOI(Query):
 class CREATE_INDEX_VERSION(Query):
 
     code = '''CREATE INDEX article_version IF NOT EXISTS FOR (n:Article) ON (n.version);'''
+
+
+class FIND_RESPONSE(Query):
+    code = '''MATCH (r:Response {related_article_doi: $related_article_doi}) RETURN r;'''
+    map = {'related_article_doi': ['related_article_doi', '']}
+    returns = ['r']
+
+
+class FIND_REVIEW(Query):
+    code = '''MATCH (r:Review {related_article_doi: $related_article_doi, review_idx: $review_idx}) RETURN r;'''
+    map = {
+        'related_article_doi': ['related_article_doi', ''],
+        'review_idx': ['review_idx', ''],
+    }
+    returns = ['r']
