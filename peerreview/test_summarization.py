a928df291d49f6b8e3041e327942b53b8e72b20b
Thomas Eidens <thomas.eidens@embo.org>
2023-03-28 17:46:52 +0200

    Added simple db test for MECADOI import

--- peerreview/test_summarization.py
+++ peerreview/test_summarization.py
@@ -1,38 +1,8 @@
-from os import getenv
-from unittest import TestCase
 from unittest.mock import call, patch
 
-from neotools.db import Instance, Query
+from neotools.db import Query
 import peerreview.summarization
-
-
-class DbTestCase(TestCase):
-    class DeleteAll(Query):
-        code = "MATCH (n) DETACH DELETE n"
-
-    class NumNodesInDatabase(Query):
-        code = "MATCH (n) RETURN COUNT(n) AS N"
-        returns = ["N"]
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.db = Instance(
-            getenv("NEO_URI"), getenv("NEO_USERNAME"), getenv("NEO_PASSWORD")
-        )
-
-    def setUp(self):
-        self.verify_database_empty()
-
-    def tearDown(self):
-        self.db.query(self.__class__.DeleteAll())
-
-    def verify_database_empty(self):
-        num_nodes_in_database = self.db.query(self.__class__.NumNodesInDatabase())[0][0]
-        self.assertEqual(
-            0,
-            num_nodes_in_database,
-            msg="The database must empty before running tests.",
-        )
+from tests.utils import DbTestCase
 
 
 review_texts = {
