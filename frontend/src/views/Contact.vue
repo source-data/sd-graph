6a0d9ff4443c7c50b5d9f606f73d57919cf68e03
Kiri Diaconu <kiri.diaconu@67bricks.com>
2023-11-27 10:58:35 +0000

    EEB 10-11: Slightly change the styling of the Contact page

--- frontend/src/views/Contact.vue
+++ frontend/src/views/Contact.vue
@@ -1,4 +1,6 @@
 <template lang="pug">
-  v-container
-    p EEB is under development and is run as an experiment to push boundaries in transparency and scientific publishing. Please send us your thoughts, suggestions and feedback to: <a href="mailto:thomas.lemberger@embo.org" type="primary">thomas.lemberger@embo.org</a>
+  v-card(flat).col-6.ml-auto.mr-auto
+    v-card-title Contact
+    v-card-text.text-body-1 <!-- Is this still relevant? Should anything change here? -->
+      p EEB is under development and is run as an experiment to push boundaries in transparency and scientific publishing. Please send us your thoughts, suggestions and feedback to: <a href="mailto:thomas.lemberger@embo.org" type="primary">thomas.lemberger@embo.org</a>
 </template>
