036b17707b4e4ad41e9568dc6ff9435f827146c8
Kiri Diaconu <kiri.diaconu@67bricks.com>
2023-12-05 17:04:33 +0000

    EEB 13: A slightly improved Not Found page, with wording similar to the SourceData website

--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,4 +1,5 @@
 <template lang="pug">
-  div(style="text-align:center")
-    h1 404 Page not found
+v-span.col-6.ml-auto.mr-auto.text-center
+  h1 Oops, you've encountered an error 404
+  p It appears the page you were looking for doesn't exist. Sorry about that.
 </template>
