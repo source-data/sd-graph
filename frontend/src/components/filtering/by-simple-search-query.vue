fff2244ed303f788ead2203cbf70b1f1a2858989
Kiri Diaconu <kiri.diaconu@67bricks.com>
2023-12-06 11:59:29 +0000

    EEB 12: Unrelated - It doesn't make sense to have the loading progress displayed in the input field

--- frontend/src/components/filtering/by-simple-search-query.vue
+++ frontend/src/components/filtering/by-simple-search-query.vue
@@ -4,7 +4,6 @@ v-card(flat).flex-grow-1
   v-card-text.d-flex.d-flex-row.align-center
     v-text-field(
       v-model="currentQuery"
-      :loading="loadingRecords"
       placeholder="keywords, authors, doi"
       prepend-icon="mdi-magnify"
       @keyup.enter="addQueryAndRefresh"
