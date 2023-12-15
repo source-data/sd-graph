12835a621d7b8009e1af252c898518bcf835a8f2
Kiri Diaconu <kiri.diaconu@67bricks.com>
2023-12-15 09:37:37 +0000

    EEB 41: Add mobile filter dialog if the screen gets too small + tweaks

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 v-container(fluid).pa-0.ma-0
-  v-row.pa-3
+  v-row.d-none.d-lg-flex.pa-3
     v-col(cols=3)
       v-card.fixed-filter-panel
         v-card-text
@@ -13,10 +13,40 @@ v-container(fluid).pa-0.ma-0
     v-col(cols=9)
       v-row
         Highlights
+
+  v-row(justify="center").d-lg-none
+    v-dialog(
+      v-if="!loadingRecords"
+      v-model="showMobileFilterDialog"
+      fullscreen
+      hide-overlay
+      transition="dialog-bottom-transition")
+      template(v-slot:activator="{ on, attrs }")
+        v-btn(
+          color="primary"
+          v-bind="attrs"
+          v-on="on").mr-auto.ml-3
+          v-icon(left) mdi-filter-variant
+          | Content filters
+      v-card
+        v-toolbar(color="tertiary")
+          v-btn(icon @click="dialog = false")
+            v-icon mdi-close
+          v-toolbar-title Content filters
+        v-card-text
+          v-col.mt-3
+            v-row
+              SearchBar
+            v-row
+              v-divider
+            v-row
+              ByReviewingService
+    Highlights
 </template>
 
 
 <script>
+import { mapState } from 'vuex'
 import Highlights from '../components/highlights/index.vue'
 import ByReviewingService from '../components/filtering/by-reviewing-service.vue'
 import SearchBar from '../components/filtering/by-simple-search-query.vue'
@@ -28,10 +58,24 @@ export default {
     ByReviewingService,
     SearchBar
   },
+  data() {
+    return {
+      showMobileFilterDialog: false
+    }
+  },
+  computed: {
+    ...mapState('byFilters', ['loadingRecords']),
+  },
   props: {
     collection: String,
     service: String,
   },
+  watch: {
+    loadingRecords(isLoading) {
+      // if a load starts while the dialog is showing, stop displaying the dialog
+      this.showMobileFilterDialog = this.showMobileFilterDialog && !isLoading
+    }
+  }
 }
 </script>
 
