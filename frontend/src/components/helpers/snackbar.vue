31b01c090de9075f9e14dad5c0af1f7d09394096
Kiri Diaconu <kiri.diaconu@67bricks.com>
2023-12-18 17:10:42 +0000

    EEB 42: Add files I forgot to add before

---
+++ frontend/src/components/helpers/snackbar.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+v-snackbar(v-model='show' :top="true" :color='color' timeout="3000" centered)
+  b {{ message }}
+  template(v-slot:action="{ attrs }")
+    v-btn(
+      text
+      v-bind="attrs"
+      @click="show = false" icon)
+        v-icon mdi-close
+</template>
+
+<script>
+  import { mapState } from 'vuex'
+
+  export default {
+    data() {
+      return {
+        show: false,
+
+        message: "",
+        color: ""
+      };
+    },
+    computed: {
+      ...mapState(['snackMessage', 'snackColor']),
+    },
+    watch: {
+      snackMessage(nv, ov) {
+        if (nv != "" && nv != ov) {
+          this.show = true;
+          this.message = this.snackMessage;
+          this.color = this.snackColor;
+
+          this.$store.commit("setSnack", { message: "", color: "" });
+        }
+      }
+    }
+  };
+</script>
