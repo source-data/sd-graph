da1421662ecbe89f596eb13738e26aafcad603a2
Kiri Diaconu <kiri.diaconu@67bricks.com>
2023-12-18 10:10:49 +0000

    EEB 16: Add journal filter component

---
+++ frontend/src/components/filtering/by-published-in.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+v-card(flat).flex-grow-1
+  v-card-title Filter by journal
+  v-card-subtitle Filter the reviewed preprints by the journal they were published in
+  v-card-text
+    v-autocomplete(
+      v-model="selectedPublishers"
+      :items="publishers.map(p => p.id)"
+      :disabled="loadingRecords"
+      label="Select publishers"
+      multiple outlined deletable-chips compact
+      chips).pa-0.mt-0
+</template>
+
+<script>
+
+import { mapState } from 'vuex'
+
+export default {
+  data () {
+    return {}
+  },
+  computed: {
+    ...mapState('byFilters', ['publishers', 'loadingRecords', 'published_in']),
+
+    selectedPublishers: {
+      set(publishers) {
+        this.$store.commit("byFilters/setPublishedIn", publishers);
+      },
+      get() {
+        return this.published_in
+      }
+    }
+  },
+  methods: {},
+}
+</script>
